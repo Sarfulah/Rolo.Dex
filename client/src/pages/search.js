ce9d6e0032b767d9035cc190f13dbe21dd32029e
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-09-05 10:53:31 -0400

    my search file

---
+++ client/src/pages/search.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+// import ReactDOM from "react-dom";
+// import API from "../utils/API";
+import Nav from "../components/Nav";
+import "./style.css";
+
+class Search extends Component {
+    state = {
+        results: []
+    };
+
+    render() {
+        return (
+            <div className="container-fluid">
+                <Nav/>
+                <div className="row">
+                    <div className="col-6">
+                        <input class="form-control" type="text" placeholder="Search" aria-label="Search" />
+                    </div>
+                    <div className="col-12">
+                        <h1>Mapped data here</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Search;
