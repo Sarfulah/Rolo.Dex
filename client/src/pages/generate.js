3ce2f9d8b8328cff2883c9931a462b7522a0da9b
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-09-06 13:31:36 -0400

    console logging all fo the form input data success

--- client/src/pages/generate.js
+++ client/src/pages/generate.js
@@ -1,17 +1,72 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import "./style.css";
 import Form from "../components/Form";
 
 class Generate extends Component {
-    state = { show: false };
+    state = { 
+       firstName: "", 
+       lastName: "",
+       mobileNum: "",
+       emailAdd: "",
+       companyName: "",
+       address: "",
+       city: "",
+       state: "",
+       zip: "",
+       website: "",
+    };
 
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+        console.log({ [name]: value})
+      };
+    
+    //   handleOnClick = event => {
+    //     console.log("I clicked saved")
+    
+    //   }
+      handleFormSubmit = event => {
 
+        const { firstName, lastName, mobileNum, emailAdd, companyName, address, city, state, zip, website } = this.state;
+        event.preventDefault();
+        // console.log ("submit")
+        this.setState({ 
+            firstName: firstName, 
+            lastName: lastName, 
+            mobileNum: mobileNum,
+            emailAdd: emailAdd,
+            companyName: companyName,
+            address: address,
+            city: city,
+            state: state,
+            zip: zip,
+            website: website,
+        })
+        console.log(this.state.firstName);
+        console.log(this.state.lastName);
+        console.log(this.state.mobileNum);
+        console.log(this.state.emailAdd);
+        console.log(this.state.companyName);
+        console.log(this.state.address);
+        console.log(this.state.city);
+        console.log(this.state.state);
+        console.log(this.state.zip);
+        console.log(this.state.website);
+
+      }
 
     render() {
         return (
             <div>
-                <Form/>
+                <Form
+                // value={this.state.firstName}
+                handleFormSubmit={this.handleFormSubmit}
+                handleInputChange={this.handleInputChange}
+                />
             </div>
         );
     }
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
-
+const routes = require('./routes');
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,6 +11,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(routes);
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/rolodex");
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
